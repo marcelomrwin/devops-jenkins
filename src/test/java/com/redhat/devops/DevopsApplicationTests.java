d677405d2d141560293d6994e8448dd689c9ba31
Marcelo Sales <marcelo.d.sales@gmail.com>
2019-12-22 00:13:10 -0300

    add source files

---
+++ src/test/java/com/redhat/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.devops;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
